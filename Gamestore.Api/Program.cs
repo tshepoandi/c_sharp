63e5b9e1e25084ef8324798b93a69e558330938a
tshepoandi <lovekilledtshepo@gmail.com>
2024-08-18 19:52:58 +0200

    games endpoint

--- Gamestore.Api/Program.cs
+++ Gamestore.Api/Program.cs
@@ -1,6 +1,42 @@
+using GameStore.Api.Entities;
+
+List<Game> games = new()
+{
+    new Game()
+    {
+        Id = 1,
+        Name = "Street Fighter",
+        Genre = "Fighting",
+        Price = 19.99m,
+        ReleaseDate = new DateTime(1991,2,1),
+        ImageUri = "",
+
+    },
+    new Game()
+    {
+        Id = 2,
+        Name = "Street Fighter 2",
+        Genre = "Role Playing",
+        Price = 29.99m,
+        ReleaseDate = new DateTime(1991,2,1),
+        ImageUri = ""
+    },
+    new Game()
+    {
+        Id = 3,
+        Name = "Captain Commando",
+        Genre = "Fighting",
+        Price = 10.50m,
+        ReleaseDate = new DateTime(1991,2,1),
+        ImageUri = ""
+        
+    },
+};
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
+app.MapGet("/games", ()=> games);
 
 app.Run();
--- Gamestore.Api/obj/Debug/net8.0/Gamestore.Api.AssemblyInfo.cs
+++ Gamestore.Api/obj/Debug/net8.0/Gamestore.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Gamestore.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6b65ea15986947cd656d6a2901759852eb6bb3ae")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9cd6db62feabf21884e0b3ec8549eb0505b0b25d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Gamestore.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Gamestore.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
