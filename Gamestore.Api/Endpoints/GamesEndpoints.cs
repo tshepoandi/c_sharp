cb3c7def842699729e96b84032b47a5a41e94a6c
tshepoandi <lovekilledtshepo@gmail.com>
2024-08-23 14:16:37 +0200

    repositories

--- Gamestore.Api/Endpoints/GamesEndpoints.cs
+++ Gamestore.Api/Endpoints/GamesEndpoints.cs
@@ -10,41 +10,7 @@ namespace Gamestore.Api.Endpoints
     {
          public static RouteGroupBuilder MapGamesEndpoints(this IEndpointRouteBuilder routes)
          {
-            const string GetGameEndpointName = "getGame"; 
-
-            List<Game> games = new()
-            {
-                new Game()
-                {
-                    Id = 1,
-                    Name = "Street Fighter",
-                    Genre = "Fighting",
-                    Price = 19.99m,
-                    ReleaseDate = new DateTime(1991,2,1),
-                    ImageUri = "",
-
-                },
-                new Game()
-                {
-                    Id = 2,
-                    Name = "Street Fighter 2",
-                    Genre = "Role Playing",
-                    Price = 29.99m,
-                    ReleaseDate = new DateTime(1991,2,1),
-                    ImageUri = ""
-                },
-                new Game()
-                {
-                    Id = 3,
-                    Name = "Captain Commando",
-                    Genre = "Fighting",
-                    Price = 10.50m,
-                    ReleaseDate = new DateTime(1991,2,1),
-                    ImageUri = ""
-                    
-                },
-            };
-            
+            const string GetGameEndpointName = "getGame";  
             var group =  routes.MapGroup("/games")
                 .WithParameterValidation();
 
---
+++ Gamestore.Api/Repositories/InMemGamesRepository.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Gamestore.Api.Repositories
+{
+    public class InMemGamesRepository
+    {
+        List<Game> games = new()
+            {
+                new Game()
+                {
+                    Id = 1,
+                    Name = "Street Fighter",
+                    Genre = "Fighting",
+                    Price = 19.99m,
+                    ReleaseDate = new DateTime(1991,2,1),
+                    ImageUri = "",
+
+                },
+                new Game()
+                {
+                    Id = 2,
+                    Name = "Street Fighter 2",
+                    Genre = "Role Playing",
+                    Price = 29.99m,
+                    ReleaseDate = new DateTime(1991,2,1),
+                    ImageUri = ""
+                },
+                new Game()
+                {
+                    Id = 3,
+                    Name = "Captain Commando",
+                    Genre = "Fighting",
+                    Price = 10.50m,
+                    ReleaseDate = new DateTime(1991,2,1),
+                    ImageUri = ""
+                    
+                },
+            };
+    }
+}
